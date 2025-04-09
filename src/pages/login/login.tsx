3dd0919d7d955818cced366cc3aeaa89b03c3baa
teimosoRC <suinagato@gmail.com>
2025-04-09 14:01:35 +0300

    create func login/reg user

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,12 +1,21 @@
 import { FC, SyntheticEvent, useState } from 'react';
 import { LoginUI } from '@ui-pages';
+import { useDispatch } from '@store';
+import { login } from '@slices/userSlice';
+import { useNavigate } from 'react-router-dom';
 
 export const Login: FC = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-  const handleSubmit = (e: SyntheticEvent) => {
+  const handleSubmit = async (e: SyntheticEvent) => {
     e.preventDefault();
+    try {
+      await dispatch(login({ email, password })).unwrap();
+      navigate('/profile', { replace: true });
+    } catch (_) {}
   };
 
   return (
