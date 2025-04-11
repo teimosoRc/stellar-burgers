5bfb6a5717ea8e4e31cdc88dac338af3a8885706
teimosoRC <suinagato@gmail.com>
2025-04-11 13:53:47 +0300

    change redirect

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -2,19 +2,22 @@ import { FC, SyntheticEvent, useState } from 'react';
 import { LoginUI } from '@ui-pages';
 import { useDispatch } from '@store';
 import { login } from '@slices/userSlice';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 export const Login: FC = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const location = useLocation();
+
+  const { from } = location.state || { from: { pathname: '/' } };
 
   const handleSubmit = async (e: SyntheticEvent) => {
     e.preventDefault();
     try {
       await dispatch(login({ email, password })).unwrap();
-      navigate('/profile', { replace: true });
+      navigate(from.pathname, { replace: true });
     } catch (_) {}
   };
 
